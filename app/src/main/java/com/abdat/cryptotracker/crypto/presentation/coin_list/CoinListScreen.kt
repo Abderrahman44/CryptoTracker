1e9a25591d34c63fe09dadd56ae5f259a3be7cec
Abderrahman abdat <abderahmanabdat@gmail.com>
2025-02-17 14:10:34 +0100

    Coin Screen

---
+++ app/src/main/java/com/abdat/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,11 @@
+package com.abdat.cryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CoinListScreen(
+    modifier: Modifier = Modifier,
+) {
+
+}
